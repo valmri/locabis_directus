66550f0ae2414cfdd72c956407f7afe14a3ae4e7
marmie <valentin.marmie@outlook.com>
2022-03-30 11:03:09 +0200

    Initialisation index.php

--- index.php
+++ index.php
@@ -4,19 +4,18 @@ require_once './vue/elements/head.php';
 
 require_once './controleur/controleurPrincipal.php';
 
-if(isset($_GET['page']) && !empty($_GET['page'])) {
+if(isset($_GET['page'])) {
 
     // Nom page
     $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
 
-    $controleur = controleur($page);
-
-    require_once './controleur/'.$controleur;
-
 } else {
 
-    require_once './vue/v_accueil.php';
+    $page = "accueil";
 
 }
+
+$controleur = controleur($page);
+require_once './controleur/'.$controleur;
 
 require_once './vue/elements/footer.php';
