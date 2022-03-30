984fd3d1e3052105c7d913b453dd1f553f34717a
marmie <valentin.marmie@outlook.com>
2022-03-30 12:37:00 +0200

    Ajout grille de logements

--- vue/v_accueil.php
+++ vue/v_accueil.php
@@ -1,15 +1,64 @@
 <body>
 
     <div class="contenu">
+
+        <aside class="infos">
+
+            <div class="info">
+
+                <div class="enteteInfo">
+                    <h2>Bienvenue sur Locabis !</h2>
+                </div>
+
+                <div class="contenuInfo">
+                    <p>Consultez dès aujourd'hui nos offres de location pour vos prochaines vacances.</p>
+                </div>
+
+            </div>
+
+        </aside>
     
         <div class="page">
 
             <div class="entetePage">
-                <h1>Accueil</h1>
+                <h1>Nos locations</h1>
             </div>
 
-            <div class="pageContenu">
-                <p>Bienvenue sur le site de location !</p>
+            <div class="contenuCarte">
+
+                <div class="carteLoc">
+                    <div class="carteLoc_entete">
+                        <img src="https://www.maisonsclairlogis.fr/wp-content/uploads/maison-contemporaine_onyx-version-nuit.jpg" width="100%" alt="">
+                    </div>
+                    <div class="carteLoc_contenu">
+                        <h2>Titre</h2>
+                        <p>Lieu</p>
+                        <p>Prix</p>
+                    </div>
+                </div>
+
+                <div class="carteLoc">
+                    <div class="carteLoc_entete">
+                        <img src="https://www.maisonsclairlogis.fr/wp-content/uploads/maison-contemporaine_onyx-version-nuit.jpg" width="100%" alt="">
+                    </div>
+                    <div class="carteLoc_contenu">
+                        <h2>Titre</h2>
+                        <p>Lieu</p>
+                        <p>Prix</p>
+                    </div>
+                </div>
+
+                <div class="carteLoc">
+                    <div class="carteLoc_entete">
+                        <img src="https://www.maisonsclairlogis.fr/wp-content/uploads/maison-contemporaine_onyx-version-nuit.jpg" width="100%" alt="">
+                    </div>
+                    <div class="carteLoc_contenu">
+                        <h2>Titre</h2>
+                        <p>Lieu</p>
+                        <p>Prix</p>
+                    </div>
+                </div>
+
             </div>
 
         </div>
